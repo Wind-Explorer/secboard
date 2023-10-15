0e3b1064bb710ced6bb63552b6d72452df36a0a2
Adam C <66894537+Wind-Explorer@users.noreply.github.com>
2023-10-15 13:15:17 +0800

    Initialize modules

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+mod sb_clipboard;
+mod sb_config;
+mod sb_crypto;
+mod sb_util;
+
 fn main() {
     println!("SecBoard!");
+    sb_clipboard::test();
+    sb_crypto::test();
+    sb_config::test();
+    sb_util::test();
 }
